f9e1f6f52de085eb346f3aa6954849d5432ba60d
Ksenia-gra <kseniiagrafova@icloud.com>
2023-02-02 22:00:06 +0300

    mvvm

---
+++ MVVM_lab_IndTask/MVVM_lab_IndTask/SquareMatrixModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+
+namespace MVVM_lab_IndTask
+{
+    internal class SquareMatrixModel : INotifyPropertyChanged
+    {
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private int size;
+        public int Size { get { return size; } set { size = value; OnPropertyChanged("Matrix"); } }
+       
+
+        public string Matrix
+        {
+
+
+            get
+            {
+                
+                Random random = new Random();
+
+                StringBuilder stringBuilder = new StringBuilder();
+
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        stringBuilder.Append(random.Next(0, 1000) + "  ");
+                    }
+                    stringBuilder.Append("\n");
+                }
+                return stringBuilder.ToString();
+            }
+            
+        }
+    }
+}
---
+++ MVVM_lab_IndTask/MVVM_lab_IndTask/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_lab_IndTask
+{
+     class ViewModel
+    {
+        public SquareMatrixModel model { get; set; }
+
+        public ViewModel()
+        {
+            model = new SquareMatrixModel();
+        }
+    }
+}
